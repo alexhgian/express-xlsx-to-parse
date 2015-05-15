14376f8f7d0aedc984220ada2acd1bbdfdfeb38c
Alex Gian <alexhgian@gmail.com>
2015-05-15 14:36:16 -0700

    Added Header

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'hjs');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Parse-Session-Token");
   next();
 });
 
