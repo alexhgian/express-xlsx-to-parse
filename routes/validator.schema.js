fcd18999c7e14702fa59dedadb62cb75a783eede
xxvii27 <llesmana@ucsd.edu>
2015-08-29 23:13:49 -0700

    2.0

--- routes/validator.schema.js
+++ routes/validator.schema.js
@@ -3,134 +3,125 @@
 //    2 = high priority
 
 module.exports = {
-  Attendee: {
-    Experience: String,
-    Title: {
-      type: String,
-      requiredLevel: 1
-    },
-    Name: {
-      type: String,
-      requiredLevel: 2
-    },
-    Email: {
-      type: String,
-      requiredLevel: 2
-    },
-    IsContactable: {
-      type: String,
-      requiredLevel: 2
-    },
-    Bio: {
-      type: String,
-      requiredLevel: 1
-    },
-    Organization: {
-      type: String,
-      requiredLevel: 1
+    Attendee: {
+        ProfessionalTitle: String,
+        Name: {
+            type: String,
+            requiredLevel: 2
+        },
+        Email: {
+            type: String,
+            requiredLevel: 2
+        },
+        IsContactable: {
+            type: String,
+            requiredLevel: 2
+        },
+        Bio: {
+            type: String,
+            requiredLevel: 1
+        },
+        Organization: {
+            type: String,
+            requiredLevel: 1
+        }
     },
     Session: {
-      type: String,
-      requiredLevel: 2
+        Track: {
+            type: String,
+            requiredLevel: 2
+        },
+        StartTime: {
+            type: String,
+            requiredLevel: 2
+        },
+        EndTime: {
+            type: String,
+            requiredLevel: 2
+        },
+        Moderator: {
+            type: String,
+            requiredLevel: 1
+        },
+        Location: {
+            type: String,
+            requiredLevel: 1
+        }
     },
-    ImageFileName: String
-  },
-  Session: {
-    Track: {
-      type: String,
-      requiredLevel: 2
+    Event: {
+        Talk: {
+            type: String,
+            requiredLevel: 2
+        },
+        Track: {
+            type: String,
+            requiredLevel: 2
+        },
+        StartTime: {
+            type: String,
+            requiredLevel: 2
+        },
+        EndTime: {
+            type: String,
+            requiredLevel: 2
+        },
+        Speakers: {
+            type: String,
+            requiredLevel: 2
+        }
     },
-    StartTime: {
-      type: String,
-      requiredLevel: 2
+    Speaker: {
+        ProfessionalTitle: {
+            type: String,
+            requiredLevel: 1
+        },
+        Name: {
+            type: String,
+            requiredLevel: 2
+        },
+        Email: {
+            type: String,
+            requiredLevel: 2
+        },
+        IsContactable: {
+            type: String,
+            requiredLevel: 2
+        },
+        Bio: {
+            type: String,
+            requiredLevel: 1
+        },
+        Organization: {
+            type: String,
+            requiredLevel: 1
+        }
     },
-    EndTime: {
-      type: String,
-      requiredLevel: 2
+    Sponsor: {
+        Name: {
+            type: String,
+            requiredLevel: 2
+        },
+        Sponsoring: {
+            type: String,
+            requiredLevel: 1
+        },
+        Website: {
+            type: String,
+            requiredLevel: 1
+        }
     },
-    Moderator: {
-      type: String,
-      requiredLevel: 1
-    },
-    Location: {
-      type: String,
-      requiredLevel: 1
+    TravelBusiness: {
+        businessName: {
+            type: String,
+            requiredLevel: 2
+        },
+        address: String,
+        businessType: {
+            type: String,
+            requiredLevel: 2
+        },
+        otherDetails: String,
+        ReservationAndRates: String,
+        website: String
     }
-  },
-  Event: {
-    Talk: {
-      type: String,
-      requiredLevel: 2
-    },
-    Track: {
-      type: String,
-      requiredLevel: 2
-    },
-    StartTime: {
-      type: String,
-      requiredLevel: 2
-    },
-    EndTime: {
-      type: String,
-      requiredLevel: 2
-    },
-    Speakers: {
-      type: String,
-      requiredLevel: 2
-    }
-  },
-  Speaker: {
-    ProfessionalTitle: {
-      type: String,
-      requiredLevel: 1
-    },
-    Name: {
-      type: String,
-      requiredLevel: 2
-    },
-    Email: {
-      type: String,
-      requiredLevel: 2
-    },
-    IsContactable: {
-      type: String,
-      requiredLevel: 2
-    },
-    Bio: {
-      type: String,
-      requiredLevel: 1
-    },
-    Organization: {
-      type: String,
-      requiredLevel: 1
-    }
-  },
-  Sponsor: {
-    Name: {
-      type: String,
-      requiredLevel: 2
-    },
-    Sponsoring: {
-      type: String,
-      requiredLevel: 1
-    },
-    Website: {
-      type: String,
-      requiredLevel: 1
-    }
-  },
-  TravelBusiness: {
-    businessName: {
-      type: String,
-      requiredLevel: 2
-    },
-    address: String,
-    businessType: {
-      type: String,
-      requiredLevel: 2
-    },
-    otherDetails: String,
-    ReservationAndRates: String,
-    website: String
-  }
 };
