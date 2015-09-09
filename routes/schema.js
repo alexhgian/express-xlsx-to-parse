d9273374526bbbea54e16ce880ac0f32a464c360
xxvii27 <llesmana@ucsd.edu>
2015-09-09 09:48:27 -0700

    relation event-spk

--- routes/schema.js
+++ routes/schema.js
@@ -67,7 +67,7 @@ exports.Schema = {
         speakers : {
             type : 'Relation',
             pointerTo : 'Attendee',
-            query : 'name'
+            query : 'email'
         }
     },
     Session : {
